bb586a522ef0877d6e40bc9c5d5599f8bcd3d2c2
Hugo Carneiro <hugofmealt@gmail.com>
2019-11-03 19:54:02 -0300

    add chai, change test location, first mock test successful

--- test/test.js
+++ test/test.js
@@ -4,15 +4,23 @@ Como os metodos em UsuarioController sao todos assincronos, a gente vai deixar
 eles para depois, e como Validation so lida com estruturas de dados, vamos
 para verifyToken
 */
-const jwt = require("jsonwebtoken");
-var verify = require('../controllers/verifyToken.js');
-var should = require('chai').should();//Tbm tem should e expect
-
-describe('Controller Tests', function(){    //Iniciando teste para o controller
+//const jwt = require("jsonwebtoken");
+//var verify = require('../controllers/verifyToken.js');
+var should = require("chai").should();//Tbm tem should e expect
+var assert = require("chai").assert;
+/*describe('Controller Tests', function(){    //Iniciando teste para o controller
   describe('VerifyToken Tests', function(){  //Iniciando teste para Validation.js
     it('Should return a token', function(done){
       const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
       should.exist(token);
+    });
+  });
+});
+*/
+describe("test", function(){
+  describe("test", function(){
+    it("should fucking work", function(){
+      assert.equal(1, 1);
     });
   });
 });
