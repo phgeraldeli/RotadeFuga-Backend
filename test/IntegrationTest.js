c5d4cf6557347d55034ac89e2a085ba969cee67b
Hugo Carneiro <hugofmealt@gmail.com>
2019-11-06 22:12:25 -0300

    Introduzindo hook pra dar uma limpa no server teste

--- test/IntegrationTest.js
+++ test/IntegrationTest.js
@@ -7,6 +7,7 @@ para verifyToken
 
 process.env.NODE_ENV = 'test'; //Set env variable to test
 
+const Usuario = require('../src/models/Usuario.js');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../src/server.js');
@@ -17,13 +18,13 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('Requests de Usuario', function(){
-/*  beforeEach((done) => { //Before each test we empty the database
-        usuarioController.delete({}, (err) => {
+/*  beforeEach(function(done){ //Before each test we empty the database
+        Usuario.remove({}, (err) => {
            done();
         });
     });*/
   describe('/Get all users', function(){
-    it('should return an empty array of users', function(done){
+    it('should return an array of users', function(done){
       chai.request(server)
           .get('/usuario')
           .end(function(err, res){
@@ -32,6 +33,22 @@ describe('Requests de Usuario', function(){
             //expect(res.body.length).to.be.eql(0);
             done();
           });
+    });
+    describe('/Post', function(){
+      it('should create a new user', function(done){
+        let user = {
+          nome: 'user',
+          latitude: 123,
+          longitude: 321
+        }
+        chai.request(server);
+            .post('/usuario')
+            .send(user)
+            .end(function(err, res){
+              expect(res).to.have.status(200);
+
+            });
+      });
     });
   });
 });
